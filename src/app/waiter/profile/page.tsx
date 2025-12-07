df07df5138e85b6742cb35e1e89111f2632d8ccd
evolutionnn <evrimdoga.eds@gmail.com>
2025-12-07 18:55:57 +0300

    waiter page table sayfası mock hald eeklendi

---
+++ src/app/waiter/profile/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+export default function WaiterNotificationsPage() {
+  return (
+    <div className="px-6 pt-12 pb-6">
+      <h1 className="text-3xl font-normal text-black mb-8" style={{ fontFamily: 'Pontano Sans, sans-serif' }}>
+        Bildirimler
+      </h1>
+
+      {/* İçerik şu an için boş bırakıldı */}
+      <div className="flex flex-col gap-4">
+        {/* Bildirim kartları buraya gelecek */}
+      </div>
+    </div>
+  );
+}
